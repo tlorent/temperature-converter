2ea3da1b519fcc0b3e7090d484ddbd2a30474bf1
Tim Lorent <timlorent@Tims-MacBook-Pro.local>
2017-06-08 11:38:35 +0200

    Change style, add background images, change script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
+  var imageURL = './assets/img/freeze1-min.jpg'
+  var imageURL2 = './assets/img/cold2-min.jpg'
+  var imageURL3 = './assets/img/medium1-min.jpg'
+  var imageURL4 = './assets/img/hot1-min.jpg'
+  var imageURL5 = './assets/img/heat1-min.jpg'
+  var imageURL6 = './assets/img/heat2-min.jpg'
+
   $("#celsiusForm").submit(function(event) {
+
     //prevent the form from refreshing automatically
     event.preventDefault();
 
@@ -14,12 +22,20 @@ $(document).ready(function() {
     //display the conversion
     $("#fahrenheit").val(convertedToFahrenheit);
 
-    var imageURL = '../img/cold1-min.jpg'
-
     //display a background image for the temperature
-    if(celsius < 0) {
+    if(celsius <= 0) {
       // $("html").css({"background-image": "url(../img/freeze1-min.jpg);"})
       $("html").css('background-image', 'url(' + imageURL + ')');
+    } else if (celsius <= 10) {
+      $("html").css('background-image', 'url(' + imageURL2 + ')');
+    } else if (celsius <= 25) {
+      $("html").css('background-image', 'url(' + imageURL3 + ')');
+    } else if (celsius <= 40) {
+      $("html").css('background-image', 'url(' + imageURL4 + ')');
+    } else if (celsius <= 70) {
+      $("html").css('background-image', 'url(' + imageURL5 + ')');
+    } else if (celsius <= 99999999999999) {
+      $("html").css('background-image', 'url(' + imageURL6 + ')');
     }
 
   });
@@ -33,10 +49,26 @@ $(document).ready(function() {
     var fahrenheit = $("#fahrenheit2").val();
 
     //convert it to Celsius
-    var convertToCelsius = (fahrenheit - 32) * (5/9);
+    var convertedToCelsius = (fahrenheit - 32) * (5/9);
 
     //display the conversion
-    $("#celsius2").val(convertToCelsius)
+    $("#celsius2").val(convertedToCelsius)
+
+    //display a background image for the temperature
+    if(convertedToCelsius <= 0) {
+      // $("html").css({"background-image": "url(../img/freeze1-min.jpg);"})
+      $("html").css('background-image', 'url(' + imageURL + ')');
+    } else if (convertedToCelsius <= 10) {
+      $("html").css('background-image', 'url(' + imageURL2 + ')');
+    } else if (convertedToCelsius <= 25) {
+      $("html").css('background-image', 'url(' + imageURL3 + ')');
+    } else if (convertedToCelsius <= 40) {
+      $("html").css('background-image', 'url(' + imageURL4 + ')');
+    } else if (convertedToCelsius <= 70) {
+      $("html").css('background-image', 'url(' + imageURL5 + ')');
+    } else if (convertedToCelsius <= 99999999999999) {
+      $("html").css('background-image', 'url(' + imageURL6 + ')');
+    }
 
   });
 
